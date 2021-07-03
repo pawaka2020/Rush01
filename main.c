5eeb5f9ea02668370f0429083106a051cb1d943b
Rinor Ramli <40174427+pawaka2020@users.noreply.github.com>
2021-07-03 02:41:42 +0000

    whatever

--- main.c
+++ main.c
@@ -18,7 +18,7 @@ int main(int args, char *argv[]) {
   // write(1, solution[1], 1);
   // write(1,  solution[2], 1);
   //...
-  // write(1, "solution[15]", 1);
+  // write(1, "solution[15]", 1);?????
 
 
 
